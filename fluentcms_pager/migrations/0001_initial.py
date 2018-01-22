33106f32965a1015565a77c0f5ed5e315b7b6293
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-22 14:30:01 +0100

    Fixed Django 2.0 support

--- fluentcms_pager/migrations/0001_initial.py
+++ fluentcms_pager/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PagerItem',
             fields=[
-                ('contentitem_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='fluent_contents.ContentItem')),
+                ('contentitem_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='fluent_contents.ContentItem', on_delete=models.CASCADE)),
                 ('previous_title', models.CharField(max_length=200, null=True, verbose_name='Title previous link', blank=True)),
                 ('previous_url', fluent_contents.extensions.PluginUrlField(max_length=300, null=True, verbose_name='URL previous link', blank=True)),
                 ('next_title', models.CharField(max_length=200, null=True, verbose_name='Title next link', blank=True)),
